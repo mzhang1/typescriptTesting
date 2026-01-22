7e4327c889ba589971ec62a90c39419e04da8fd8
Michael <zhangmichael167@gmail.com>
2026-01-23 00:12:09 +0100

    Update : Added back BaseCardInfos type

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,10 @@
+import { Color } from "./enums"
+
 export type DescriptionMetadata = {
     [key: string]: string | number | null
+}
+export type BaseCardInfos = {
+    originalName: string,
+    colorIdentity: Color,
+    convertedManaCost: number
 }
