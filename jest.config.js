5f26902b5a72294a5c106549c719843b43905797
mzhang1 <zhangmichael167@gmail.com>
2026-01-26 17:20:52 +0100

    Config update : Now testing is mapping @ directories correctly

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,10 @@ module.exports = {
   transform: {
     ...tsJestTransformCfg,
   },
+  moduleDirectories: [
+    'node_modules'
+  ],
+  "moduleNameMapper": {
+    "^@/(.*)": "<rootDir>/src/$1"
+  }
 };
