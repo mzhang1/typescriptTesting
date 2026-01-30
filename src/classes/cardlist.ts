76627bc6a0a809c8a11ea80c6232a6c405cd5b6f
Michael <zhangmichael167@gmail.com>
2026-01-30 23:49:33 +0100

    Update : Implemented CardList.add

--- src/classes/cardlist.ts
+++ src/classes/cardlist.ts
@@ -2,14 +2,20 @@ import { Card } from "@/classes/card";
 
 export class CardList{
     static #instance: CardList;
+    current: Array<Card>;
 
     private constructor() {}
 
     public static get() : CardList {
         if(!CardList.#instance){
             CardList.#instance = new CardList();
+            CardList.#instance.current = [];
         }
 
         return CardList.#instance;
+    }
+
+    public static add(newCard: Card) {
+        CardList.#instance.current.push(newCard);
     }
 }
