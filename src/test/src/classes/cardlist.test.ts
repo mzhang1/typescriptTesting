7e978027988ae140342d45b9c06c6d8d485eabcd
Michael <zhangmichael167@gmail.com>
2026-01-30 23:39:25 +0100

    Test Update : added new test case

--- src/test/src/classes/cardlist.test.ts
+++ src/test/src/classes/cardlist.test.ts
@@ -1,6 +1,17 @@
+import { Card } from "@/classes/card";
 import { CardList } from "@/classes/cardlist";
+import { Color } from "@/enums";
+
 import type * as types from "@/types";
 
 test("CardList should be accessible at any point", () => {
     expect(CardList.get()).toBeTruthy();
+})
+
+test("CardList.add should work", () => {
+    expect(CardList.add(new Card({
+        originalName: "Emptiness",
+        colorIdentity: Color.White | Color.Black,
+        convertedManaCost: 6
+    }))).not.toThrow(Error);
 })
