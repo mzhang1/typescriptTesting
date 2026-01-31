2de57d246b91d1ffb47cd926934b9777a5caec33
Michael <zhangmichael167@gmail.com>
2026-01-31 21:49:12 +0100

    Update : Using PlaneswalkerCardInfos from the new file

--- src/classes/planeswalkercard.ts
+++ src/classes/planeswalkercard.ts
@@ -1,12 +1,12 @@
 import { Card } from "@/classes/card"
 import type { BaseCardInfos } from "@/types";
 
+import type { PlaneswalkerCardInfos } from "@/types/cardtypes"
+
 export class PlaneswalkerCard extends Card{
     readonly startingLoyalty: number;
 
-    constructor(cardParams: BaseCardInfos, customTypeInfos: {
-        startingLoyalty: number
-    }){
+    constructor(cardParams: BaseCardInfos, customTypeInfos: PlaneswalkerCardInfos){
         super(cardParams);
         this.startingLoyalty = customTypeInfos.startingLoyalty;
     }
