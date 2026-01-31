b19d57cf50ec2dd69c0766402fd53bf1175b6df3
Michael <zhangmichael167@gmail.com>
2026-01-31 21:49:56 +0100

    Test update : Adding new card

--- src/test/src/classes/cardlist.test.ts
+++ src/test/src/classes/cardlist.test.ts
@@ -1,5 +1,6 @@
 import { Card } from "@/classes/card";
 import { CardList } from "@/classes/cardlist";
+import { PlaneswalkerCard } from "@/classes/planeswalkercard";
 import { Color } from "@/enums";
 
 import type * as types from "@/types";
@@ -17,4 +18,15 @@ test("CardList.add should work", () => {
     CardList.add(card);
 
     expect(CardList.get().current.length).toBe(1);
+
+    const planeswalkerCard = new PlaneswalkerCard({
+        originalName: "The Royal Scions",
+        colorIdentity: Color.Blue | Color.Red,
+        convertedManaCost: 3
+    },
+    {
+        startingLoyalty: 5
+    });
+    CardList.add(planeswalkerCard);
+    expect(CardList.get().current.length).toBe(2);
 })
