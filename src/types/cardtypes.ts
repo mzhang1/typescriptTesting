534bfaa98edc1de969667636029f9ee72901d005
Michael <zhangmichael167@gmail.com>
2026-01-31 21:48:34 +0100

    Update : Added new PlaneswalkerCardInfos type

--- src/types/cardtypes.ts
+++ src/types/cardtypes.ts
@@ -2,3 +2,6 @@ export type CreatureCardInfos = {
     readonly power: string | number,
     readonly toughness: string | number
 };
+export type PlaneswalkerCardInfos = {
+    readonly startingLoyalty: number
+};
